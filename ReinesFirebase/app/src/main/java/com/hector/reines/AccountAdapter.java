65aa1bcedb129a5f9c450a6e46b4908ae248a909
unknown <lewdxample>
2019-08-07 11:18:28 +0700

    Firebase Integrated

---
+++ ReinesFirebase/app/src/main/java/com/hector/reines/AccountAdapter.java
@@ -0,0 +1,87 @@
+package com.hector.reines;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class AccountAdapter extends RecyclerView.Adapter<AccountAdapter.AccountViewHolder> {
+
+    private DatabaseReference mGetRef;
+    private Account account;
+
+    private ArrayList<Account> accountList;
+
+    public void setAccountList(ArrayList<Account> accountList) {
+        this.accountList = accountList;
+    }
+
+    public AccountAdapter(ArrayList<Account> accountList) {
+        this.accountList = accountList;
+    }
+    @NonNull
+    @Override
+    public AccountAdapter.AccountViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.saved_account, parent, false);
+        return new AccountViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AccountAdapter.AccountViewHolder holder, int pos) {
+        holder.txt_gameName.setText(accountList.get(pos).getGame_name());
+        holder.txt_uid.setText("UID : " +accountList.get(pos).getUid());
+        holder.txt_genId.setText("Generated ID : " +accountList.get(pos).getGen_id());
+        holder.txt_passCode.setText("Pass Code : " +accountList.get(pos).getPass_code());
+    }
+
+    @Override
+    public int getItemCount() {
+        return (accountList != null) ? accountList.size() : 0;
+    }
+
+    public class AccountViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView txt_gameName, txt_uid, txt_genId, txt_passCode;
+        private Button delBtn;
+
+        public AccountViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txt_gameName = itemView.findViewById(R.id.txt_gameName);
+            txt_uid = itemView.findViewById(R.id.txt_uid);
+            txt_genId = itemView.findViewById(R.id.txt_genID);
+            txt_passCode = itemView.findViewById(R.id.txt_passCode);
+            delBtn = itemView.findViewById(R.id.deleteAcc);
+
+            delBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    removeItem(getAdapterPosition());
+//                    Log.e("adapterpos", "onClick: "+accountList.get(getAdapterPosition()).getAcc_id());
+//                    Log.e("adapterpos", "pos: "+getAdapterPosition());
+
+                }
+            });
+        }
+
+        public void removeItem(int pos){
+            mGetRef = FirebaseDatabase.getInstance().getReference().child("archives").child("ver1");
+            mGetRef.child(accountList.get(pos).getAcc_id()).removeValue();
+//            accountList.remove(pos);
+//            notifyItemRemoved(pos);
+            notifyDataSetChanged();
+        }
+    }
+}
---
+++ ReinesFirebase/app/src/main/java/com/hector/reines/MainActivity.java
@@ -0,0 +1,105 @@
+package com.hector.reines;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    DatabaseReference mDatabase, mGetRef;
+
+    private MaterialEditText input_genId, input_passCode, input_Uid;
+    private Spinner input_gameName;
+    private Button saveBtn;
+
+    private RecyclerView accountRecycler;
+    private AccountAdapter adapter;
+    private ArrayList<Account> accountArrayList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mGetRef = mDatabase.child("archives").child("ver1");
+
+        input_Uid = findViewById(R.id.materialEditText0);
+        input_genId = findViewById(R.id.materialEditText);
+        input_passCode = findViewById(R.id.materialEditText2);
+        input_gameName = findViewById(R.id.spinner);
+        saveBtn = findViewById(R.id.save_btn);
+
+        accountArrayList = new ArrayList<>();
+
+        accountRecycler = findViewById(R.id.accountRecy);
+        adapter = new AccountAdapter(accountArrayList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+        accountRecycler.setLayoutManager(layoutManager);
+        accountRecycler.setAdapter(adapter);
+
+        mGetRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                accountArrayList.clear();
+
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()){
+                    Log.e("data", "data : " +dataSnapshot1.getValue());
+                    accountArrayList.add(new Account(
+                            dataSnapshot1.child("acc_id").getValue().toString(),
+                            dataSnapshot1.child("game_name").getValue().toString(),
+                            dataSnapshot1.child("uid").getValue().toString(),
+                            dataSnapshot1.child("gen_id").getValue().toString(),
+                            dataSnapshot1.child("pass_code").getValue().toString()));
+                }
+
+                adapter.setAccountList(accountArrayList);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addToDB();
+                input_Uid.setText("");
+                input_genId.setText("");
+                input_passCode.setText("");
+            }
+        });
+    }
+
+    void addToDB(){
+        Account account = new Account("", "", "", "", "");
+        account.setAcc_id(mGetRef.push().getKey());
+        account.setGame_name(input_gameName.getSelectedItem().toString());
+        account.setUid(input_Uid.getText().toString());
+        account.setGen_id(input_genId.getText().toString());
+        account.setPass_code(input_passCode.getText().toString());
+        mGetRef.child(account.getAcc_id()).setValue(account);
+    }
+}
